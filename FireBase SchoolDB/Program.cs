db42dfcd84d55e0a2ddee34511b02c0b4f952d66
Sebastian Petersen <sebkirller@gmail.com>
2019-03-05 09:13:36 +0100

    Added program to get data from FireBase

---
+++ FireBase SchoolDB/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace FireBase_SchoolDB
+{
+    class Program
+    {
+        const string URI = "https://schooldb-8ea56.firebaseio.com/";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSubjects());
+        }
+
+        private static Dictionary<string, dynamic> GetSubjects()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(URI + "Subject/.json").Result;
+                Dictionary<string, dynamic> data =
+                    JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(response.Content.ReadAsStringAsync().Result);
+
+                return data;
+            }
+        }
+    }
+}
